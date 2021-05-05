ca4b807ff5a5d619341e359143547ea1235de0c4
Paulo Ferreira <paulo_fleite@outlook.com>
2021-05-04 23:20:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function cadastrar() {
+  console.log("tamo indo fera")
+}
+
+function activeCategoria() {
+  const categoria = document.getElementById("categoria")
+console.log(categoria)
+  categoria.classList.remove('esconder')
+}
