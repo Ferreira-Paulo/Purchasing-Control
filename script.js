27470d39ae1141965f345db87c8497c096f18971
Paulo Ferreira <paulo_fleite@outlook.com>
2021-05-05 00:15:06 -0300

    started page Cadastro (commit on 'master')

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ function cadastrar() {
   console.log("tamo indo fera")
 }
 
-function activeCategoria() {
+/*function activeCategoria() {
   const categoria = document.getElementById("categoria")
 console.log(categoria)
   categoria.classList.remove('esconder')
+}*/
+
+function visualizarImage() {
+  const link = document.getElementById("url_image").value
+  document.getElementById('previwer_image').style.background = `url('${link}')`
+  document.getElementById('previwer_image').style.backgroundSize = `cover`
+  document.getElementById('previwer_image').style.opacity = `1`
+  document.getElementById('previwer_image').style.height += "120px"
+}
+
+function limparImage() {
+  document.getElementById('previwer_image').style = ''
 }
